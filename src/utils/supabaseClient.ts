87a2b58a6775cde7d979ce0f6564af78adca5dbd
Alex <alex@MacBook-Pro-Alex.local>
2025-04-02 17:11:06 +0300

    Change url and key for database

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'http://127.0.0.1:54321'; // локальный URL
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0'; // твой локальный anon key
+const supabaseUrl = 'https://unlxqleauxykssskzzsq.supabase.co'; // локальный URL
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVubHhxbGVhdXh5a3Nzc2t6enNxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDI4MzEwMTEsImV4cCI6MjA1ODQwNzAxMX0.ZjcSGhEzc0Xy9WqwHwUaIE9M8WZUyR6x4wflm7x7hUo'; // твой локальный anon key
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
